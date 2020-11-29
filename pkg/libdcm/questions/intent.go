a6298ea5e5342e4543181ba1609a3803bbd34d2e
4thel00z <4thel00z@gmail.com>
2020-11-29 03:47:53 +0100

    [master] Add survey.Questions for intent model

---
+++ pkg/libdcm/questions/intent.go
@@ -0,0 +1,28 @@
+package questions
+
+import "github.com/AlecAivazis/survey/v2"
+
+var (
+	FirstIntent = []*survey.Question{
+		{
+			Name: "name",
+			Prompt: &survey.Input{
+				Message: "What is the name of your Intent?",
+			},
+			Validate: survey.Required,
+		},
+		{
+			Name: "entityFiller",
+			Prompt: &survey.Input{
+				Message: "What is the name of the entityFiller reprompt? (skip this if no reprompt is needed)",
+			},
+		},
+		{
+			Name: "fallbackToNoSession",
+			Prompt: &survey.Confirm{
+				Message: "Do you want to fallback to the session for entity retrieval?",
+				Default: false,
+			},
+		},
+	}
+)
