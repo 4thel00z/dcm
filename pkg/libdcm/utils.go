1043077165949942569cec9d4430c7aa47a08d94
4thel00z <4thel00z@gmail.com>
2020-11-29 03:47:13 +0100

    [master] Add utils to make my life easier using the survey package

---
+++ pkg/libdcm/utils.go
@@ -0,0 +1,85 @@
+package libdcm
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+	"strings"
+)
+
+func Confirm(m string) (confirm bool, err error) {
+	err = survey.AskOne(&survey.Confirm{
+		Message: m,
+	}, &confirm)
+	return
+}
+func ConfirmWithHelp(m, help string) (confirm bool, err error) {
+	err = survey.AskOne(&survey.Confirm{
+		Message: m,
+		Help:    help,
+	}, &confirm)
+	return
+}
+
+func SelectMultipleStringArrays(continuePrompt, multiSelectPrompt, help string, choices ...string) (target [][]string, err error) {
+	target = [][]string{}
+	p := &survey.MultiSelect{
+		Message: multiSelectPrompt,
+		Options: choices,
+		Help:    help,
+	}
+
+	for {
+		var current []string
+		confirmation, err := Confirm(continuePrompt)
+		if err != nil {
+			return nil, err
+		}
+		if !confirmation {
+			break
+		}
+
+		err = survey.AskOne(p, &current)
+		if err != nil {
+			return nil, err
+		}
+		target = append(target, current)
+	}
+
+	return
+}
+
+func SelectMultipleStrings(continuePrompt, multiSelectPrompt, help string, choices ...string) (target []string, err error) {
+	target = []string{}
+	p := &survey.MultiSelect{
+		Message: multiSelectPrompt,
+		Options: choices,
+		Help:    help,
+	}
+
+	if continuePrompt != "" {
+		confirmation, err := Confirm(continuePrompt)
+		if err != nil {
+			return nil, err
+		}
+		if !confirmation {
+			return nil, err
+		}
+	}
+
+	err = survey.AskOne(p, &target)
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
+
+func Multilines(prompt string) []string {
+	content := ""
+	err := survey.AskOne(&survey.Multiline{
+		Message: prompt,
+	}, &content)
+	if err != nil {
+		return []string{}
+	}
+	return strings.Split(content, "\n")
+}
